78eff0dd8b97b52e56e97ad4d7b08a88af29f7a5
Krzysztof Kielak <krzysztof.kielak@gmail.com>
2025-06-01 11:41:53 +0200

    updates from fedora

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,4 @@
 -- set to `true` to follow the main branch
 -- you need to have a working rust toolchain to build the plugin
 -- in this case.
-vim.g.lazyvim_blink_main = true
+-- vim.g.lazyvim_blink_main = true
